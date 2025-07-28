29ba222adb2b29f684d271e1e1839840e70deb9d
zfadel989 <zfadel989@gmail.com>
2025-07-28 00:35:44 -0500

    adding all commonents

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
 import Navbar from "./components/Navbar"
 import Header from "./components/Header"
 import About from "./components/About"
-import Services from "./components/Services"
-import Teams from "./components/Teams"
+import Service from "./components/Services"
+import Team from "./components/Teams"
 import Project from "./components/Project"
 import Testimonial from "./components/Testmonial"
-import Blog from "./components/Blog"
-import FAQ from "./components/FAQ"
 import Contact from "./components/Contact"
 import Footer from "./components/Footer"
-
-
 
 function App() {
 
   return (
     <>
-     <Navbar />
-     <Header />
-     <About />
-     <Services />
-     <Teams />
-     <Project />
-     <Testimonial />
-     <FAQ/>
-     {/* <Blog/> */}
-     <Contact />
-     <Footer />
-
+    <Navbar />
+    <Header />
+    <About />
+    <Service />
+    <Team />
+    <Project />
+    <Testimonial />
+    <Contact />
+    <Footer />
     </>
-  );
+  )
 }
 
 export default App
